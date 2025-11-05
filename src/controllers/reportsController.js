90711ba827332dc747fb1e87a92f63b9e4c1b5e2
afonsolelis <afonsolelis@gmail.com>
2025-11-05 20:02:56 -0300

    Relatórios: loga erro no servidor para /relatorios/tags-mensal

--- src/controllers/reportsController.js
+++ src/controllers/reportsController.js
@@ -12,9 +12,10 @@ router.get('/tags-mensal', async (req, res) => {
     const { rows } = await query(tagsMonthlySql, [year]);
     return res.render('reports/tagsMonthly', { year, rows });
   } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Erro em /relatorios/tags-mensal:', err);
     return res.status(500).send('Erro ao carregar relatório mensal por tag.');
   }
 });
 
 module.exports = { router };
-
